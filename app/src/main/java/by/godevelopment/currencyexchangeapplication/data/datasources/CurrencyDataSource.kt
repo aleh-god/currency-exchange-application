8fb54567e1a7343c18744694fbce7364b0834803
aleh-god <aleh.god@gmail.com>
2022-10-09 13:03:00 +0300

    add: RemoteData files and logic.

--- app/src/main/java/by/godevelopment/currencyexchangeapplication/data/datasources/CurrencyDataSource.kt
+++ app/src/main/java/by/godevelopment/currencyexchangeapplication/data/datasources/CurrencyDataSource.kt
@@ -1,8 +1,8 @@
 package by.godevelopment.currencyexchangeapplication.data.datasources
 
-import by.godevelopment.currencyexchangeapplication.commons.TASK_INTERVAL
-import by.godevelopment.currencyexchangeapplication.data.remoteapi.CurrencyApi
 import by.godevelopment.currencyexchangeapplication.data.entities.CurrencyEntity
+import by.godevelopment.currencyexchangeapplication.data.remoteapi.CurrencyApi
+import by.godevelopment.currencyexchangeapplication.di.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -12,14 +12,15 @@ import javax.inject.Inject
 
 class CurrencyDataSource @Inject constructor(
     private val currencyApi: CurrencyApi,
+    @IoDispatcher
     private val ioDispatcher: CoroutineDispatcher,
-    private val refreshIntervalMs: Long = TASK_INTERVAL
+    private val refreshIntervalMs: Long
 ) {
-    val latestRates: Flow<List<CurrencyEntity>> = flow {
+    suspend fun fetchLatestRates(): Flow<List<CurrencyEntity>> = flow {
         while(true) {
-            val latestRates = currencyApi.fetchLatestRates()
-            emit(latestRates) // Emits the result of the request to the flow
-            delay(refreshIntervalMs) // Suspends the coroutine for some time
+            val latestRates = currencyApi.fetchRates()
+            emit(latestRates)
+            delay(refreshIntervalMs)
         }
     }.flowOn(ioDispatcher)
 }
---
+++ app/src/main/java/by/godevelopment/currencyexchangeapplication/data/repositories/CurrencyRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package by.godevelopment.currencyexchangeapplication.data.repositories
+
+import by.godevelopment.currencyexchangeapplication.data.datasources.CurrencyDataSource
+import by.godevelopment.currencyexchangeapplication.data.utils.toCurrencyModel
+import by.godevelopment.currencyexchangeapplication.domain.api.CurrencyRepository
+import by.godevelopment.currencyexchangeapplication.domain.models.CurrencyModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class CurrencyRepositoryImpl @Inject constructor(
+    private val currencyDataSource: CurrencyDataSource
+) : CurrencyRepository {
+
+    override suspend fun fetchLatestRates(): Flow<List<CurrencyModel>> = currencyDataSource
+        .fetchLatestRates()
+        .map { list ->
+            list
+                .filter { it.base != null && it.rate != null }
+                .map { it.toCurrencyModel() }
+        }
+}
