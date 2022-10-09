d247a4ff47e67a930a98de5a33cc6211d0cd80fd
aleh-god <aleh.god@gmail.com>
2022-10-09 12:59:52 +0300

    add: Api interface and item model.

--- app/src/main/java/by/godevelopment/currencyexchangeapplication/domain/api/CurrencyRepository.kt
+++ app/src/main/java/by/godevelopment/currencyexchangeapplication/domain/api/CurrencyRepository.kt
@@ -1,4 +1,9 @@
 package by.godevelopment.currencyexchangeapplication.domain.api
 
+import by.godevelopment.currencyexchangeapplication.domain.models.CurrencyModel
+import kotlinx.coroutines.flow.Flow
+
 interface CurrencyRepository {
+
+    suspend fun fetchLatestRates(): Flow<List<CurrencyModel>>
 }
